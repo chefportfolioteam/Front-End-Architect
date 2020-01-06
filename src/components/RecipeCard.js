3d58cd4f0257176a38d7c10a3c97af66439f6a33
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2020-01-06 14:31:19 -0600

    fixed logic on recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -2,13 +2,17 @@ import React, { useContext, useEffect } from "react";
 import {AuthContext} from'../Contexts/AuthContext'
 import {EditContext} from'../Contexts/EditContext' 
 import { Link } from 'react-router-dom';
+import ChefDash from "./UserDash";
+
 import UserDash from "./UserDash";
+import EditRecipe from "./EditRecipe";
+
 
 
 export const RecipeCard = props => {
    
     
-    const {deleteRecipe, editinfo, recipe, setRecipes, cancelItem,  } = useContext(AuthContext)
+    const {deleteRecipe, editinfo, recipe, recipes, cancelItem,  } = useContext(AuthContext)
     
  useEffect (() => {
     editinfo(props.match.params.id)
@@ -27,19 +31,19 @@ if(!recipe){
         <div>
             <EditContext.Provider value={{recipe}} >
             <div>
-            <button onClick={cancelItem} >Cancel</button>
+            
                 <span>{recipe.recipe_name}</span>
                 <p>{recipe.ingredients}</p>
                 <p>{recipe.instructions}</p>
-               
+                <button onClick={cancelItem} >Cancel</button>
             </div>
 
-               {!UserDash &&    
+               {localStorage.getItem('token') &&
                 
                 <Link to={`/edit-recipe/${props.match.params.id}`}>Update</Link>
              
                 }
-                {!UserDash && 
+                {localStorage.getItem('token') &&
                 <button onClick={e => {deleteRecipe(props.match.params.id);
                
                 props.history.push('/chefdash');
