9504d1872fc17df75b30a57f9aaf55015ab8c77c
LISA_COOK <lisabpink@gmail.com>
2020-01-09 12:33:28 -0500

    Lisa testing

--- src/components/MealType.js
+++ src/components/MealType.js
@@ -1,4 +1,4 @@
-//Lisa Test 5
+//Lisa Test 6
 import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
